2e04cf71920845d1650a620a0f178f57e4d0729f
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2021-12-13 10:46:22 +0100

    Create Project
    🎊 🎉

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import static com.company.RemisePrix.prixFinale;
+import static com.company.RemisePrix.paiement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        paiement(20,prixFinale(50,30));
+    }
+
+}
